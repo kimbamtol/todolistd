3386ac604f02ca9429a7ff50a6c31717fe3f7e46
kimbamtol0304 <kimbamtol0304@gmail.com>
2023-11-15 21:33:29 +0900

    Update Body.js

--- Body.js
+++ Body.js
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Modal, TextInput } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 class Body extends Component {
+  state = {
+    modalVisible: false,
+    newBssid: '', // 추가
+  };
+
+  setModalVisible = (visible) => {
+    this.setState({ modalVisible: visible });
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -18,11 +28,44 @@ class Body extends Component {
               </TouchableOpacity>
               <Text style={data.completed ? styles.todoCompleted : null}>{data.text}</Text>
             </View>
-            <TouchableOpacity onPressOut={() => this.props.deleteTodo(data.id)}>
-              <MaterialCommunityIcons style={styles.todoDelBtn} size={30} name="delete-outline" />
-            </TouchableOpacity>
+            <View style={styles.todoButtons}>
+              <TouchableOpacity onPressOut={() => this.props.deleteTodo(data.id)}>
+                <MaterialCommunityIcons style={styles.todoDelBtn} size={30} name="delete-outline" />
+              </TouchableOpacity>
+              <TouchableOpacity onPressOut={() => this.setModalVisible(true)}>
+                <Ionicons style={styles.todoAddBtn} size={30} name="md-wifi-outline" />
+              </TouchableOpacity>
+            </View>
           </View>
         ))}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            this.setModalVisible(!this.state.modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Enter BSSID:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="BSSID"
+                onChangeText={(newBssid) => this.setState({ newBssid })}
+              />
+              <TouchableOpacity
+                style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                  // 여기서 newBssid를 저장하거나 다른 작업을 수행
+                }}
+              >
+                <Text style={styles.textStyle}>Save BSSID</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -53,10 +96,58 @@ const styles = StyleSheet.create({
   },
   todoDelBtn: {
     color: '#777',
+    marginRight: 10,
+  },
+  todoAddBtn: {
+    color: '#4169E1',
+  },
+  todoButtons: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   todoCompleted: {
     color: '#bbb',
     textDecorationLine: 'line-through',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginTop: 10,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
